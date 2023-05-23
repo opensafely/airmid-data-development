fa6624c04a96011c55e998c979cf9a46e536643b
Iain Dillingham <iain@dillingham.me.uk>
2023-05-23 10:30:13 +0100

    Create frequency table of patients by surveys
    Create a frequency table of the number of patients that completed the
    given number of surveys (consultations).

--- analysis/consultation_id/query.sql
+++ analysis/consultation_id/query.sql
@@ -1,8 +1,17 @@
--- How many patients and how many rows are associated with each consultation?
+-- Create a frequency table of the number of patients that completed the given number of
+-- surveys (consultations).
 SELECT
-    Consultation_ID AS consultation_id,
-    COUNT(DISTINCT Patient_ID) AS num_patients,
-    COUNT(Patient_ID) AS num_rows
-FROM OpenPROMPT
-GROUP BY Consultation_ID
-ORDER BY Consultation_ID
+    num_consultations,
+    COUNT(Patient_ID) AS num_patients
+FROM (
+    -- Each row represents a patient's response to a question. Questions are grouped
+    -- into surveys (consultations). How many surveys (consultations) were completed by
+    -- each patient?
+    SELECT
+        Patient_ID,
+        COUNT(DISTINCT Consultation_ID) AS num_consultations
+    FROM OpenPROMPT
+    GROUP BY Patient_ID
+) AS t
+GROUP BY num_consultations
+ORDER BY num_consultations

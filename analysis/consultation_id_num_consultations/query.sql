0be54e4aaa58b32dd94b8f87f6c8db2192320425
Iain Dillingham <iain@dillingham.me.uk>
2023-05-26 16:47:22 +0100

    Create frequency table of surveys by dates
    Create a frequency table of the number of surveys (consultations) that
    are administered on the given number of dates.

---
+++ analysis/consultation_id_num_consultations/query.sql
@@ -0,0 +1,21 @@
+-- Create a frequency table of the number of surveys (consultations) that are
+-- administered on the given number of dates. If the result is that all surveys
+-- (consultations) are administered on one date, then we can infer that Consultation_ID
+-- is an FK.
+SELECT
+    num_dates,
+    COUNT(num_dates) AS num_consultations
+FROM (
+    SELECT
+        Consultation_ID,
+        COUNT(Consultation_ID) AS num_dates
+    FROM (
+        -- unique pairs
+        SELECT DISTINCT
+            Consultation_ID,
+            ConsultationDate
+        FROM OpenPROMPT
+    ) AS t
+    GROUP BY Consultation_ID
+) AS t
+GROUP BY num_dates

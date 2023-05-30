7009b3de4d3281da753a150f2611ae8be4b9a83c
Iain Dillingham <iain@dillingham.me.uk>
2023-05-30 16:26:20 +0100

    Revert "Count CodedEvent_ID"
    This reverts commit d8f747efe72c25417af317a6867d48bcb3f295f2.

--- analysis/code_system_id/query.sql
+++ analysis/code_system_id/query.sql
@@ -1,11 +1,8 @@
--- Do values of code system ID map to values of concept ID? How may patients,
--- rows, and concept IDs are associated with each mapping? If one concept ID is
--- associated with each mapping, then we can assume that the IDs are references to the
--- same underlying concepts.
+-- Do values of code system ID map to values of concept ID? How may patients and how
+-- many rows are associated with each mapping?
 SELECT
     CodeSystemId AS code_system_id,
     ConceptId AS concept_id,
-    COUNT(DISTINCT CodedEvent_ID) AS num_coded_event_ids,
     COUNT(DISTINCT Patient_ID) AS num_patients,
     COUNT(Patient_ID) AS num_rows
 FROM OpenPROMPT

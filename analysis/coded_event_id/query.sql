04308c3facd5d9d99f2646e367368616226b6186
Iain Dillingham <iain@dillingham.me.uk>
2023-06-19 10:39:46 +0100

    Use column names from the ehrQL table
    To make things slightly easier, we use column names from the ehrQL table
    (`open_prompt`) rather than from the database table (`OpenPROMPT`).

--- analysis/coded_event_id/query.sql
+++ analysis/coded_event_id/query.sql
@@ -1,7 +1,7 @@
 -- Are values of CodedEvent_ID valid CTV3 codes?
 SELECT
-    CodedEvent_ID AS coded_event_id,
-    Description AS description,
+    CodedEvent_ID AS ctv3_code,
+    Description AS ctv3_description,
     MIN(ConsultationDate) AS min_consultation_date
 FROM OpenPROMPT LEFT JOIN CTV3Dictionary ON CodedEvent_ID = CTV3Code
 GROUP BY CodedEvent_ID, Description

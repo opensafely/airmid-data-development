e39c44e7dc5884f26765bae3aae6fad283c8c7cc
Iain Dillingham <iain@dillingham.me.uk>
2023-06-19 10:25:07 +0100

    Return minimum ConsultationDate by CodedEvent_ID
    As requested, @hendersonad!

--- analysis/coded_event_id/query.sql
+++ analysis/coded_event_id/query.sql
@@ -1,6 +1,8 @@
 -- Are values of CodedEvent_ID valid CTV3 codes?
-SELECT DISTINCT
+SELECT
     CodedEvent_ID AS coded_event_id,
-    Description AS description
+    Description AS description,
+    MIN(ConsultationDate) AS min_consultation_date
 FROM OpenPROMPT LEFT JOIN CTV3Dictionary ON CodedEvent_ID = CTV3Code
+GROUP BY CodedEvent_ID, Description
 ORDER BY CodedEvent_ID
